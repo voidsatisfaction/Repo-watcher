e4dd634120f5304e66f4b90f33a944f64e1b2679
voidSatisfaction <lourie@naver.com>
2018-01-31 15:02:25 +0900

    refactor: change mailer using builder pattern

---
+++ src/pkg/mailer/mailer.go
@@ -0,0 +1,39 @@
+package mailer
+
+import (
+	"Repo-watcher/src/pkg/config"
+	"fmt"
+	"net/smtp"
+)
+
+type Mailer struct {
+	From       string
+	Username   string
+	Password   string
+	To         []string
+	smtpServer string
+	auth       smtp.Auth
+}
+
+func New(c *config.Config) *Mailer {
+	smtpServer := "smtp.gmail.com:587"
+	host := "smtp.gmail.com"
+	auth := smtp.PlainAuth("", c.Mail.Username, c.Mail.Password, host)
+	return &Mailer{
+		c.Mail.From,
+		c.Mail.Username,
+		c.Mail.Password,
+		c.Mail.To,
+		smtpServer,
+		auth,
+	}
+}
+
+func (m *Mailer) Send(body string) error {
+	err := smtp.SendMail(m.smtpServer, m.auth, m.From, m.To, []byte(body))
+	if err != nil {
+		fmt.Printf("smtp error: %+v\n", err)
+		return err
+	}
+	return nil
+}
